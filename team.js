d820168331c640c0c37ffa09732881e33a9bb9a8
JSter91 <129691071+JSter91@users.noreply.github.com>
2023-10-31 16:46:06 +0100

    oggetto aggiunto

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let myArray =[];
+let myArray =[
+    {
+        name:"Salvatore",
+        surname:"Marotta",
+        age:32,
+        city:"Licata",
+        hobby:"play the guitar",
+        favoriteFood:"pizza",
+        favoriteBook:"padre ricco padre povero",
+        petName:"Nino"
+    }
+];
