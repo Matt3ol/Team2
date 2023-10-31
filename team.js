d41e7fe58bb670d199b7e9e3a4e7fa6ec0ec508f
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2023-10-31 16:26:33 +0100

    Update team.js

--- team.js
+++ team.js
@@ -1,15 +1,14 @@
-let Fabrizio =
-[
-    {
-name:"Fabrizio",
-surname: "Pala",
-age: "29",
-city:"Torino",
-hobby:"Anime",
-favoriteFood:"Pollo fritto",
-favoriteVideoGame:"Skyrim",
-favoriteFilm:"Forrest Gump",
-favoriteBook:"IT",
-petName:"haru",
-}
-];
+let Fabrizio =[
+ {
+   name:"Fabrizio",
+   surname: "Pala",
+   age: "29",
+   city:"Torino",
+   hobby:"Anime",
+   favoriteFood:"Pollo fritto",
+   favoriteVideoGame:"Skyrim",
+   favoriteFilm:"Forrest Gump",
+   favoriteBook:"IT",
+   petName:"haru",
+ }
+]
