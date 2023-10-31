5328552bf25f4b587878fefdd405135525e3dc53
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2023-10-31 15:57:49 +0100

    scrittura java script  team.js

--- team.js
+++ team.js
@@ -1,1 +1,15 @@
-let myArray =[];
+let myArray =
+[
+    {
+name:"Fabrizio",
+surname: "Pala",
+age: "29",
+city:"Torino",
+hobby:"Anime",
+favoriteFood:"Pollo fritto",
+favoriteVideoGame:"Skyrim",
+favoriteFilm:"Forrest Gump",
+favoriteBook:"IT",
+petName:"haru",
+}
+];
