d0ba9bb5b2cea6054bf1bb17c6176ae2fbb4d71c
Matteo <Matteoshurdhaqi@gmail.com>
2023-10-31 16:42:38 +0100

    commit oggetto

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let myArray =[];
+let myArray =[
+    {
+        name: "Matteo",
+        surname: "Shurdhaqi",
+        age: 22,
+        city: "Frosolone",
+        hobby: "",
+        favFood: "",
+        favVideogame: "",
+        favFilm: "",
+        favBook: "",
+        petNam: "Attila",
+    }
+];
