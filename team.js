0c3d2cf668b74bd3f5a9eda263e68a4df47773ff
RaulZahariuc <raulzahariuc@gmail.com>
2023-11-01 19:22:15 +0100

    added new information on team.js

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-let myArray =[];
+let myArray =[    {
+    name: "Raul",
+    surname: "Zahariuc",
+    age: 21,
+    city: "Roma",
+    hobby: "e-Sport",
+    favFood: "Sushi",
+    favVideogame: "Jak and Dexter",
+    favFilm: "Wiplash",
+    favBook: "Uno studio in rosso",
+    petNam: "",
+}
+];
