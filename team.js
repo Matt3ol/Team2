9f8665ab7143ec46130f9508a70d8fc3897bc19d
Matteo <Matteoshurdhaqi@gmail.com>
2023-11-02 14:54:51 +0100

    merge completato

--- team.js
+++ team.js
@@ -1,5 +1,4 @@
 let myArray =
-<<<<<<< HEAD
     [ 
     { name : "Matteo",
         surname :"Leacche",
@@ -60,85 +59,25 @@ let myArray =
     favoriteBook: "padre ricco padre povero",
     petName: "Nino",
   },
+  {
+    name: "Matteo",
+    surname: "Shurdhaqi",
+    age: "22",
+    city: "Roma",
+    hobby: "coding",
+    favoriteFood: "pasta arrabbiata",
+    favoriteVideoGame: "dark souls",
+    favoriteFilm: "Pulp Fiction",
+    favoriteBook: "C++",
+    petName: "Attila",
+
+    }
 ];
-=======
-    [
-        {
-            name: "Matteo",
-            surname: "Leacche",
-            age: "25",
-            city: "Roma",
-            hobby: "calcio",
-            favoriteFood: "pizza",
-            favoriteVideoGame: "fifa",
-            favoriteFilm: "hateful eight",
-            favoriteBook: "il sentiero dei nidi di ragno",
-            petName: "nala",
 
-        },
-        {
-            name: "Fabrizio",
-            surname: "Pala",
-            age: "29",
-            city: "Torino",
-            hobby: "Anime",
-            favoriteFood: "Pollo fritto",
-            favoriteVideoGame: "Skyrim",
-            favoriteFilm: "Forrest Gump",
-            favoriteBook: "IT",
-            petName: "haru",
-        },
-
-        {
-            name: "Junela",
-            surname: "Dedja",
-            age: 26,
-            city: "Ravenna",
-            hobby: "fotografia",
-            favFood: "",
-            favVideogame: "",
-            favFilm: "",
-            favBook: "",
-            petNam: "Dior",
-        },
-        {
-            name: "Raul",
-            surname: "Zahariuc",
-            age: 21,
-            city: "Roma",
-            hobby: "e-Sport",
-            favFood: "Sushi",
-            favVideogame: "Jak and Dexter",
-            favFilm: "Wiplash",
-            favBook: "Uno studio in rosso",
-            petNam: "",
-        },
-        {
-            name: "Salvatore",
-            surname: "Marotta",
-            age: 32,
-            city: "Licata",
-            hobby: "play the guitar",
-            favoriteFood: "pizza",
-            favoriteBook: "padre ricco padre povero",
-            petName: "Nino"
-        },
-        {
-            name: "Matteo",
-            surname: "Shurdhaqi",
-            age: "22",
-            city: "Roma",
-            hobby: "coding",
-            favoriteFood: "pasta arrabbiata",
-            favoriteVideoGame: "dark souls",
-            favoriteFilm: "Pulp Fiction",
-            favoriteBook: "C++",
-            petName: "Attila",
-
-        },
+    
+        
 
 
-    ];
 
 
->>>>>>> 2c4d4d5 (formattato e aggiunto ultimo oggetto)
+
