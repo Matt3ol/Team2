458b17811b217930c833f26c6f3e507ec7ef82e3
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2023-11-02 09:52:10 +0100

    Team2

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let myArray =[];
+let myArray =
+    [{ name : "Matteo",
+        surname :"Leacche",
+        age : "25",
+        city : "Roma",
+        hobby : "calcio",
+        favoriteFood : "pizza",
+        favoriteVideoGame : "fifa",
+        favoriteFilm : "hateful eight",
+        favoriteBook : "il sentiero dei nidi di ragno",
+        petName : "nala",
+        }];
