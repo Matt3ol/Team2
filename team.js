13c49107602c51ddda868a6b773612a05f9b7e62
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2023-11-03 10:39:21 +0100

    Update team.js

--- team.js
+++ team.js
@@ -43,7 +43,7 @@ let myArray = [
     city: "Roma",
     hobby: "e-Sport",
     favoriteFood: "Sushi",
-    favoriteVideogame: "LOL",
+    favoriteVideogame: "Jak and Dexter",
     favoriteFilm: "Wiplash",
     favoriteBook: "Uno studio in rosso",
     petName: "",
